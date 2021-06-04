eb1bcfbf65a9d5524d0d283cca73b340eca747fd
dmytro.ivanchykhin <di@o-log-n.com>
2021-06-04 09:42:43 +0300

    switching to the latest version of Global-MQ Module and updating code of test app

--- include/infrastructure/nodecpp_gmqueue_customized_types.h
+++ include/infrastructure/nodecpp_gmqueue_customized_types.h
@@ -1,6 +1,7 @@
 #ifndef NODECPP_GMQUEUE_CUSTOMIZED_TYPES_H
 #define NODECPP_GMQUEUE_CUSTOMIZED_TYPES_H
 
+#include <safememory/safe_ptr.h>
 #include <marshalling.h>
 #include <internal_msg.h>
 
@@ -12,6 +13,8 @@ public:
 	using ComposerT = globalmq::marshalling::JsonComposer<BufferT>;
 //	using ParserT = globalmq::marshalling::GmqParser<BufferT>;
 //	using ComposerT = globalmq::marshalling::GmqComposer<BufferT>;
+	template<class T>
+	using OwningPtrT = ::safememory::owning_ptr<T>;
 };
 
 
